74b33075255fe97d70684e84342bbc5481a16415
Donquellso <kamil080801@gmail.com>
2025-05-19 03:53:18 +0200

    Finished functions ceasarCipher and analyzeArray with multiple Jest Tests which in turn end Odin Project

--- functions.js
+++ functions.js
@@ -31,4 +31,47 @@ const calculator = {
     return num1 / num2;
   },
 };
-export { calculator, capitalize, reverseString };
+function caesarCipher(string, shift) {
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    let shifted = null;
+    if (isLetter(string[i])) {
+      shifted = string[i].charCodeAt(0) + shift;
+      if (isUpperCase(string[i])) {
+        if (shifted > 90) shifted -= 26;
+        if (shifted < 65) shifted += 26;
+      } else {
+        if (shifted > 122) shifted -= 26;
+        if (shifted < 97) shifted += 26;
+      }
+      result += String.fromCharCode(shifted);
+    } else {
+      result += string[i];
+    }
+  }
+  return result;
+}
+function isUpperCase(char) {
+  return char === char.toUpperCase() && char !== char.toLowerCase();
+}
+function isLetter(char) {
+  if (
+    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) ||
+    (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
+  )
+    return true;
+  return false;
+}
+function analyzeArray(arr) {
+  if (arr.length === 0) throw new Error("Array is empty");
+  const sum = arr.reduce((a, b) => {
+    return a + b;
+  }, 0);
+  return {
+    average: sum / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
+export { analyzeArray, calculator, capitalize, reverseString, caesarCipher };
--- utils.test.js
+++ utils.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString, calculator } from "./functions.js";
+import {
+  caesarCipher,
+  capitalize,
+  reverseString,
+  calculator,
+  analyzeArray,
+} from "./functions.js";
 
 test("capitalizeString", () => {
   expect(capitalize("nieporownywalny")).toBe("NIEPOROWNYWALNY");
@@ -48,17 +54,56 @@ test("calculatorMultiply", () => {
   expect(calculator.multiply(8, 4)).toBe(32);
 });
 test("ceasarCipher", () => {
-  expect(caesarCipher("xyz", 3)).toBe("abc");
-});
-test("ceasarCipher", () => {
   expect(caesarCipher("abc", 3)).toBe("def");
 });
 test("ceasarCipherWrapping", () => {
   expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+test("ceasarCipherWrappingBack", () => {
+  expect(caesarCipher("abc", -3)).toBe("xyz");
 });
 test("ceasarCipherWithNumbers", () => {
   expect(caesarCipher("ab1c", 3)).toBe("de1f");
 });
 test("ceasarCipherPunctuation", () => {
   expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+test("analyzeArray", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("analyzeArrayNegativeNums", () => {
+  expect(analyzeArray([-5, -2, -10, -1])).toEqual({
+    average: -4.5,
+    min: -10,
+    max: -1,
+    length: 4,
+  });
+});
+
+test("analyzeArrayOneElement", () => {
+  expect(analyzeArray([7])).toEqual({
+    average: 7,
+    min: 7,
+    max: 7,
+    length: 1,
+  });
+});
+
+test("analyzeArrayFloats", () => {
+  expect(analyzeArray([1.5, 2.5, 3.5])).toEqual({
+    average: 2.5,
+    min: 1.5,
+    max: 3.5,
+    length: 3,
+  });
+});
+
+test("analyzeArrayEmpty", () => {
+  expect(() => analyzeArray([])).toThrow();
 });
