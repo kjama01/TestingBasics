fffe7caab78704505440eeac4e4b8946ec4a2205
Donquellso <kamil080801@gmail.com>
2025-05-19 02:23:55 +0200

    Added reversedString function and a calculator object with multiple Jest tests.

--- functions.js
+++ functions.js
@@ -1,8 +1,34 @@
 function capitalize(string) {
   if (!string || string == "") return "";
-  string.toString();
-  return string.toUpperCase();
+  string = string.toString().toUpperCase();
+  let str = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57) continue;
+    str += string[i];
+  }
+  return str;
 }
-function reverseString(string) {}
-
-export { capitalize, reverseString };
+function reverseString(string) {
+  if (!string && string !== 0) return "";
+  string = string.toString();
+  let str = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    str += string[i];
+  }
+  return str;
+}
+const calculator = {
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
+  subtract: (num1, num2) => {
+    return num1 - num2;
+  },
+  multiply: (num1, num2) => {
+    return num1 * num2;
+  },
+  divide: (num1, num2) => {
+    return num1 / num2;
+  },
+};
+export { calculator, capitalize, reverseString };
--- utils.test.js
+++ utils.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./functions.js";
+import { capitalize, reverseString, calculator } from "./functions.js";
 
 test("capitalizeString", () => {
   expect(capitalize("nieporownywalny")).toBe("NIEPOROWNYWALNY");
@@ -12,13 +12,53 @@ test("capitalizeNull", () => {
 test("capitalizeNumber", () => {
   expect(capitalize(123)).toBe("");
 });
+test("capitalizeNumberWithLetters", () => {
+  expect(capitalize("123abc")).toBe("ABC");
+});
 test("capitalizeUndefined", () => {
   expect(capitalize(undefined)).toBe("");
 });
 
 test("reversedString", () => {
-  expect(reverseString("teststring").toBe("gnirtstset"));
+  expect(reverseString("teststring")).toBe("gnirtstset");
 });
 test("reversedNull", () => {
-  expect(reverseString(null).toBe(""));
+  expect(reverseString(null)).toBe("");
+});
+test("reversedUndefined", () => {
+  expect(reverseString(undefined)).toBe("");
+});
+test("reversedStringWithNumbers", () => {
+  expect(reverseString(12345)).toBe("54321");
+});
+test("reversedEmpty", () => {
+  expect(reverseString("")).toBe("");
+});
+
+test("calculatorAdd", () => {
+  expect(calculator.add(2, 4)).toBe(6);
+});
+test("calculatorSubtract", () => {
+  expect(calculator.subtract(8, 4)).toBe(4);
+});
+test("calculatorDivide", () => {
+  expect(calculator.divide(8, 4)).toBe(2);
+});
+test("calculatorMultiply", () => {
+  expect(calculator.multiply(8, 4)).toBe(32);
+});
+test("ceasarCipher", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+test("ceasarCipher", () => {
+  expect(caesarCipher("abc", 3)).toBe("def");
+});
+test("ceasarCipherWrapping", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+test("ceasarCipherWithNumbers", () => {
+  expect(caesarCipher("ab1c", 3)).toBe("de1f");
+});
+test("ceasarCipherPunctuation", () => {
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
